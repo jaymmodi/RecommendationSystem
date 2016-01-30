eabe3f306b361793b93994663490b3368dd57b0c
jaymmodi <modi.jay6@gmail.com>
2016-01-29 22:50:50 -0500

    adding movie dataset

--- src/Data.java
+++ src/Data.java
@@ -1,9 +1,25 @@
+import java.io.*;
+
 /**
  * Created by jay on 1/29/16.
  */
 public class Data {
     public static void main(String[] args) {
-        System.out.println("Jay");
+        System.out.println("Please give the path to training file");
+
+        try {
+            FileReader fileReader = new FileReader(new File("u1.base"));
+            BufferedReader br = new BufferedReader(fileReader);
+
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
